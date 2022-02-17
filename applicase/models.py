3d5735d687181da29590db7345abf579301ecc99
Paul Rodriguez <prod00@me.com>
2022-02-16 19:20:18 -0500

    requirements.txt

--- applicase/models.py
+++ applicase/models.py
@@ -1,10 +1,9 @@
 from typing import Tuple
-
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.html import escape, mark_safe
 
-#USER_TYPES = (("Student", "Student"), ("Professor", "Professor"))
 YEAR_TYPES: Tuple[Tuple[str, str], Tuple[str, str], Tuple[str, str], Tuple[str, str], Tuple[str, str]] = (("Freshman", "Freshman"), ("Sophomore", "Sophomore"), ("Junior", "Junior"), ("Senior", "Senior"), ("Other", "Other"))
 class User(AbstractUser):
     is_student = models.BooleanField(default=False)
@@ -27,3 +26,11 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     year = models.CharField(choices=YEAR_TYPES, max_length=50, default="Freshman")
     interests = models.ManyToManyField(Subject, related_name='interested_students')
+
+    def __str__(self):
+        return self.user.username
+
+# class PositionPost(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+#     title = models.CharField(max_length=100)
+#     date_posted = models.DateTimeField(default=timezone.now)
